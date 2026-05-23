aa29e6bef23339c1a69b99185b2d05a400ab8cf8
Rakshan <srakshan2509@gmail.com>
2026-05-23 22:29:19 +0530

    Initial RVR Academy Website

---
+++ js/script.js
@@ -0,0 +1,75 @@
+const menuToggle =
+document.querySelector(".menu-toggle");
+
+const navLinks =
+document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+  navLinks.classList.toggle("active");
+
+});
+const reveals =
+document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+  reveals.forEach((section) => {
+
+    const windowHeight =
+    window.innerHeight;
+
+    const revealTop =
+    section.getBoundingClientRect().top;
+
+    const revealPoint = 150;
+
+    if(revealTop < windowHeight - revealPoint){
+
+      section.classList.add("active");
+
+    }
+
+  });
+
+});
+const sections =
+document.querySelectorAll("section");
+
+const navItems =
+document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach((section) => {
+
+    const sectionTop =
+    section.offsetTop;
+
+    const sectionHeight =
+    section.clientHeight;
+
+    if(pageYOffset >= sectionTop - 200){
+
+      current = section.getAttribute("id");
+    }
+
+  });
+
+  navItems.forEach((link) => {
+
+    link.classList.remove("active");
+
+    if(
+      link.getAttribute("href")
+      === `#${current}`
+    ){
+
+      link.classList.add("active");
+    }
+
+  });
+
+});
