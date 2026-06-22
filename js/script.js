d6bf01a168a142300ebc054d762a2c6970eff903
Rakshan <srakshan2509@gmail.com>
2026-06-22 21:03:16 +0530

    Initial Firebase login page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,98 @@
+const menuToggle =
+document.querySelector(".menu-toggle");
+
+const navLinks =
+document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+  navLinks.classList.toggle("active");
+
+});
+const reveals =
+document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+  reveals.forEach((section) => {
+
+    const windowHeight =
+    window.innerHeight;
+
+    const revealTop =
+    section.getBoundingClientRect().top;
+
+    const revealPoint = 150;
+
+    if(revealTop < windowHeight - revealPoint){
+
+      section.classList.add("active");
+
+    }
+
+  });
+
+});
+const sections =
+document.querySelectorAll("section");
+
+const navItems =
+document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach((section) => {
+
+    const sectionTop =
+    section.offsetTop;
+
+    const sectionHeight =
+    section.clientHeight;
+
+    if(pageYOffset >= sectionTop - 200){
+
+      current = section.getAttribute("id");
+    }
+
+  });
+
+  navItems.forEach((link) => {
+
+    link.classList.remove("active");
+
+    if(
+      link.getAttribute("href")
+      === `#${current}`
+    ){
+
+      link.classList.add("active");
+    }
+
+  });
+
+});
+
+const popup =
+document.getElementById("popup");
+
+const openPopup =
+document.getElementById("openPopup");
+
+const closePopup =
+document.getElementById("closePopup");
+
+openPopup.addEventListener("click", () => {
+
+  popup.style.display = "flex";
+});
+
+closePopup.addEventListener("click", () => {
+
+  popup.style.display = "none";
+});
+
 const joinForm = document.getElementById("joinForm");
 
 joinForm.addEventListener("submit", (e) => {
